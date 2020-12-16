801e37e1f8d0ffef9f56aab8f0766efffeab1da3
Kzouggar <khaoulazouggar@gmail.com>
2020-12-16 14:57:42 +0100

    5st-commit

--- client/src/Components/steps.js
+++ client/src/Components/steps.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "../css/steps.css";
+import hc from "../photos/hc.png";
 
 function Steps() {
   return (
-    <div className='steps'>
+    <div className="steps">
       <div className="progressbar">
         <div>1</div>
         <div></div>
@@ -12,17 +13,28 @@ function Steps() {
         <div>3</div>
       </div>
       <div className="step">
-          <div className= "step1">step 1</div>
-          <div>what is your gender
-              <button>male</button>
-              <button>female</button>
-          </div>
-          <div>what are you looking for
-              <button>male</button>
-              <button>female</button>
-              <button>both</button>
-          </div>
+        <div className="s">
+          {/* <div className= "step1">step 1</div> */}
+          <p className="para">what is your gender ?</p>
+          <span className="gender">
+            <button className="genre">male</button>
+            <button className="genre">female</button>
+          </span>
+          <p className="para">what are you looking for ?</p>
+          <span className="gender">
+            <button className="genre">male</button>
+            <button className="genre">female</button>
+            {/* <button className="genre">both</button> */}
+          </span>
+          
+        </div>
 
+        <div className="photo">
+          <img src={hc} style={{ width: "275px" }} />
+          <div className="divv">
+            <button className="next">Next</button>
+          </div>
+        </div>
       </div>
     </div>
   );
