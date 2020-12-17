91d77efa37a51e982f8ab7799e15fb914399ab95
Kzouggar <khaoulazouggar@gmail.com>
2020-12-17 16:34:37 +0100

    5st-commit

---
+++ client/src/Components/instep.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Instep() {
+  return (
+    <div className= "In">
+      <p className="para">What is your gender ?</p>
+      <span className="gender">
+        <button className="genre">male</button>
+        <button className="genre">female</button>
+      </span>
+      <p className="para">What are you looking for ?</p>
+      <span className="gender">
+        <button className="genre">male</button>
+        <button className="genre">female</button>
+      </span>
+    </div>
+  );
+}
+export default Instep;
--- client/src/Components/steps.js
+++ client/src/Components/steps.js
@@ -1,72 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/steps.css";
 import hc from "../photos/hc.png";
-import list from "../photos/write.svg";
+import write from "../photos/write.svg";
+import list from "../photos/Checklist.gif";
 import { ArrowRight } from "react-feather";
 import { ArrowLeft } from "react-feather";
-
-
+import InStep from "./instep";
+import upload from "../photos/upload.svg"
+let handleImg = (nbrStep) => {
+  let srcImg;
+  if (nbrStep === 0  )
+    srcImg = hc
+  else if (nbrStep === 1)
+    srcImg = write
+  else if (nbrStep === 2)
+    srcImg = list
+  else
+    srcImg = upload
+return srcImg
+}
 function Steps() {
+  const [inStep1, setInStep] = useState(0);
   return (
-    // <div className="steps">
-    //   <div className="progressbar">
-    //     <div>1</div>
-    //     <div></div>
-    //     <div>2</div>
-    //     <div></div>
-    //     <div>3</div>
-    //   </div>
-    //   <div className="all">
-    //     <div className="step">
-    //       <div className="s">
-    //         {/* <div className= "step1">step 1</div> */}
-    //         <p className="para">what is your gender ?</p>
-    //         <span className="gender">
-    //           <button className="genre">male</button>
-    //           <button className="genre">female</button>
-    //         </span>
-    //         <p className="para">what are you looking for ?</p>
-    //         <span className="gender">
-    //           <button className="genre">male</button>
-    //           <button className="genre">female</button>
-    //           {/* <button className="genre">both</button> */}
-    //         </span>
-    //       </div>
-
-    //       <div className="photo">
-    //         <img src={hc} style={{ width: "275px" }} />
-
-    //       </div>
-    //     </div>
-    //     <div className="divv">
-    //           <button className="next">Next <ArrowRight style={{display:"flex" ,float: "right", marginTop: 2}} size={20} /></button>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="steps">
       <div className="progressbar">
         <div>1</div>
-        <div></div>
-        <div>2</div>
-        <div></div>
-        <div>3</div>
+        <div style={inStep1 === 0 ? {background:"white"} : { background: "#646bfaad"}}></div>
+        <div style={inStep1 === 0 ? {background:"white"} : { background: "#646bfaad", color:"white"}}>2</div>
+        <div style={inStep1 === 0 || inStep1 === 1 ? {background:"white"} : { background: "#646bfaad"}}></div>
+        <div style={inStep1 === 0 || inStep1 === 1 ? {background:"white"} : { background: "#646bfaad", color:"white"}}>3</div>
+        <div style={inStep1 === 0 || inStep1 === 1 || inStep1 === 2 ? {background:"white"} : { background: "#646bfaad"}}></div>
+        <div style={inStep1 === 0 || inStep1 === 1 || inStep1 === 2 ? {background:"white"} : { background: "#646bfaad", color:"white"}}>4</div>
       </div>
       <div className="all">
         <div className="step">
-          <div className="s">
-       
-            <textarea className="bio" type="text" placeholder="Add your Bio" />
-            
+          <div className="instep">
+            {inStep1 === 0  ? (<InStep />) : inStep1 === 1 ? (<textarea className="bio" type="text" placeholder="Add your Bio" />) : inStep1 === 2 ? ("step 3") : ("step 4")}
           </div>
-
           <div className="photo">
-            <img alt="" src={list} style={{ width: "380px", marginTop:40 }} />
+            {}
+            <img alt="" src={handleImg(inStep1)} style={inStep1 === 0 ? {width: '275px'} : inStep1 === 1 ? { width: '380px', marginTop:'40px' } : {width : '310px'} }  />
           </div>
         </div>
         <div className="divv">
-          <button className="previous"> <ArrowLeft style={{marginRight:9 ,display:"flex" ,float: "left", marginTop: 2}} size={20} />Previous</button>
-          <button className="next">Next <ArrowRight style={{display:"flex" ,float: "right", marginTop: 2}} size={20} /></button>
+          {inStep1 !== 0 ? (
+            <button
+              className="previous"
+              onClick={() => {
+                setInStep(inStep1 - 1);
+              }}
+            >
+              {" "}
+              <ArrowLeft
+                style={{ marginRight: 9, display: "flex", float: "left", marginTop: 2 }}
+                size={20}
+              />
+              Previous
+            </button>
+          ) : (
+            ""
+          )}
+          <button
+            className="next"
+            onClick={() => {
+              setInStep(inStep1 === 3 ? inStep1 : inStep1 + 1);
+            }}
+          >
+            Next <ArrowRight style={{ display: "flex", float: "right", marginTop: 2 }} size={20} />
+          </button>
         </div>
       </div>
     </div>
