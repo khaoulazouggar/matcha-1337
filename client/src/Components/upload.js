0d9ae0a2502f656eee1085c854b89add612069ae
Kzouggar <khaoulazouggar@gmail.com>
2020-12-22 16:32:42 +0100

    5st-commit

--- client/src/Components/upload.js
+++ client/src/Components/upload.js
@@ -2,25 +2,24 @@ import React, { useState } from "react";
 import "../css/upload.css";
 
 function Upload(props) {
-  const onDrop = (picture) => {
-    
-  let   data = URL.createObjectURL(picture[0])
-
+  const onDrop = (e, picture) => {
+    console.log('loj')
+    let data = URL.createObjectURL(picture[0]);
     props.data.setImg([data, ...props.data.img]);
+    e.target.value = "";
   };
-  console.log(props.data.img)
   return (
     <div className="upload">
       <div className="file-upload">
         <div className="image-upload-wrap">
-          <input className="file-upload-input" type="file" accept="image/*" onChange={(e) => onDrop(e.target.files)} />
+          <input className="file-upload-input" type="file" accept="image/*" onChange={(e) => onDrop(e, e.target.files)} />
           <div className="drag-text">
             <h3>Drag and drop a file or select add Image</h3>
           </div>
         </div>
-        {props.data.img.map((p) => (
+        {/* {props.data.img.map((p) => (
           <img className="file-upload-image" src={p} alt={p} key={p} />
-        ))}   
+        ))}    */}
       </div>
     </div>
   );
