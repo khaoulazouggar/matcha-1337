5a6d17b58841ed188b5d1db6cf3357863ea1b87a
Kzouggar <khaoulazouggar@gmail.com>
2021-03-18 11:34:20 +0100

    done

--- server/setup/test.js
+++ server/setup/test.js
@@ -1,5 +1,5 @@
 const data = require("./data.json");
-const db = require("./db");
+const db = require("../db");
 
 for (let i = 0; i < data.length; i++) {
   db.query("INSERT INTO `users` set ?", [data[i]], (err, res) => {
